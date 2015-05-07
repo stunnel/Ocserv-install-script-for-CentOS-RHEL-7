756eb497b6e7d56f7cc08662d860a88c089cbc2e
Travis Lee <travislee8964@users.noreply.github.com>
2015-05-08 03:04:44 +0800

    Update ocserv-update-script-for-centos-7.sh
    *更新libtasn1的版本为4.5
    *更新ocserv的版本为0.10.4

--- ocserv-update-script-for-centos-7.sh
+++ ocserv-update-script-for-centos-7.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ocserv_version=0.10.2
-version=${1-$ocserv_version}
-filename=ocserv-$version.tar.xz
-dirname=ocserv-$version
-url="ftp://ftp.infradead.org/pub/ocserv/$filename"
+ocserv_version=0.10.4
+version=${1-${ocserv_version}}
+libtasn1_version=4.5
+filename="ocserv-${version}.tar.xz"
+dirname="ocserv-${version}"
+url="ftp://ftp.infradead.org/pub/ocserv/${filename}"
 ##export LIBGNUTLS_CFLAGS="-I/usr/include/" LIBGNUTLS_LIBS="-L/usr/lib/ -lgnutls"
 
 #检测是否是root用户
@@ -21,9 +22,9 @@ if [[ $(grep "release 7." /etc/redhat-release 2>/dev/null | wc -l) -eq 0 ]]; the
 fi
 
 function updatelibtasn1 {
-    wget -t 0 -T 60 http://ftp.gnu.org/gnu/libtasn1/libtasn1-4.4.tar.gz
-    tar axf libtasn1-4.4.tar.gz
-    cd libtasn1-4.4
+    wget -t 0 -T 60 "http://ftp.gnu.org/gnu/libtasn1/libtasn1-${libtasn1_version}.tar.gz"
+    tar axf libtasn1-${libtasn1_version}.tar.gz
+    cd libtasn1-${libtasn1_version}
     ./configure --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include
     make && make install
     cd ..
@@ -36,12 +37,12 @@ case $1 in
 esac
 
 #下载ocserv并编译安装
-if [ ! -f "$filename" ]; then
-    wget -t 0 -T 60 "$url"
+if [ ! -f "${filename}" ]; then
+    wget -t 0 -T 60 "${url}"
 fi
-rm -rf "$dirname"
-tar axf $filename
-cd $dirname
+rm -rf "${dirname}"
+tar axf "${filename}"
+cd "${dirname}"
 
 sed -i 's/#define MAX_CONFIG_ENTRIES.*/#define MAX_CONFIG_ENTRIES 200/g' src/vpn.h
 ./configure
